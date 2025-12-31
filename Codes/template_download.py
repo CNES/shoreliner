b7eeca1757f88c5c0684758985fb6461e894239e
Marcan Graffin <148250755+MarcanGraffin@users.noreply.github.com>
2025-12-31 13:42:32 +0100

    Update template_download.py

--- Codes/template_download.py
+++ Codes/template_download.py
@@ -1,245 +1,171 @@
-#---------------------------------------------------------------------------------------------
-#--------------- Index maps collection (from GEE) --------------------------------------------
-#--------------- Marcan Graffin (largely inspired by CoastSat) --------------
-#---------------------------------------------------------------------------------------------
-import ee
-import time
-from urllib.request import urlretrieve
-import os
-import shutil
-import zipfile
-from osgeo import gdal, osr
 import pickle
 import numpy as np
-from shapely import geometry
-import sys
-sys.path.insert(0, r"../../../Codes/functions")
+import os
+from osgeo import gdal
 import yaml
 from yaml.loader import SafeLoader
-import GEE, Tools
+import datetime
+import sys
+sys.path.insert(0, r"../../../Codes")
+from functions import Tools,Segmentation,toSHP
+import glob
+import pandas as pd
+import geopandas as gpd
 
-
-global inputs,epsg_target
 file_inputs = 'config.yaml'
 inputs = yaml.load(open(os.path.join('../',file_inputs),'rb'),Loader = SafeLoader)
+tag_idx = inputs['WaterlineIndex']
+seg_method = inputs['Contouring']
 
-if not(inputs['Waterline'] or inputs['SandBar'] or inputs['Vegetataion']):
-    sys.exit('No Feature to extract, please modify the config.yaml file')
+path_index = os.path.join('./',tag_idx)
+ploting=False
+lag_corr=True
+waterline = []
+lags = {'S2':5,'L5':15,'L7':7.5,'L8':7.5,'L9':7.5}
 
-
-
-useS2,useL9,useL8,useL7,useL5=GEE.satMissions(inputs['Missions'])
-
-ee.Initialize(project=inputs['EE_ID'])
-
-start,end = GEE.dates(inputs['Dates'])
-
-
-
-filepath = './'
 polygon=pickle.load(open('poly.json','rb'))
 
 lonEPSG=(polygon[0][0][0]+polygon[0][2][0])/2
 latEPSG=(polygon[0][0][1]+polygon[0][2][1])/2
 epsg_target = Tools.convert_wgs_to_utm(lonEPSG, latEPSG)
 
-
-GEE.folderCreation(inputs)
-
-if inputs['Waterline']:
-    wl_f = GEE.index_f(inputs['WaterlineIndex'])
-
-if inputs['SandBar']:
-    sb_f = GEE.index_f(inputs['SandBarIndex'])
-
-if inputs['Vegetation']:
-    vg_f = GEE.index_f(inputs['VegetationIndex'])
-
-if not('RGB' in os.listdir()) and inputs['RGB']:
-    os.mkdir('RGB')
-
-global polygon_geom
-polygon_geom=ee.Geometry.Polygon(polygon)
-polygon_tmp = ee.FeatureCollection(ee.Feature(polygon_geom))
-
-if inputs['Waterline']:
-    wl_data=ee.ImageCollection([])
-if inputs['SandBar']:
-    sb_data=ee.ImageCollection([])
-if inputs['Vegetation']:
-    vg_data=ee.ImageCollection([])
-if inputs['RGB']:
-    rgb_data=ee.ImageCollection([])
-
-
-def ensureProjLandsat(image):
-    return image.reproject(
-        **{
-      'crs':'EPSG:'+epsg_target,
-      'scale': 15}).copyProperties(image,['res','date','satellite'])
-
-def ensureProjS2(image):
-    return image.reproject(
-        **{
-      'crs':'EPSG:'+epsg_target,
-      'scale': 10}).copyProperties(image,['res','date','satellite'])
-#Image Collection generation and SCoWI computation 
-#%%S2 ini
-
-if useS2:
-    S2 = ee.ImageCollection("COPERNICUS/S2_HARMONIZED")
-    S2 = S2.filterDate(start,end).filter(ee.Filter.bounds(polygon_tmp)).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",inputs['MaxCloudCover']))
-    if inputs['SpecSeasonalPeriod']:
-        S2 = S2.filter(ee.Filter.calendarRange(inputs['SpecDates'][0],inputs['SpecDates'][1],'month'))
-    S2 = S2.map(GEE.normalizeBandNamesS2)
-    S2 = GEE.resampleL5S2(S2,inputs)
-    
-    if inputs['Waterline']:
-        wl_data=wl_data.merge(S2.map(wl_f).map(ensureProjS2))
-    if inputs['SandBar']:
-        sb_data=sb_data.merge(S2.map(sb_f))
-    if inputs['Vegetation']:
-        vg_data.merge(S2.map(vg_f))
-    if inputs['RGB']:
-        rgb_data = rgb_data.merge(S2.map(GEE.RGB).map(ensureProjS2))
+#%%Process
+if inputs['NatureROI']=='Transects':
+    transects = pickle.load(open('transects.p','rb'))
     
     
+    for i in transects:
+        tmp1 = [transects[i]['transect'][:][0][0],transects[i]['transect'][0][1]]
+        tmp2 = [transects[i]['transect'][:][1][0],transects[i]['transect'][1][1]]
+        transects[i]['transect'] = np.array([tmp1,tmp2])
     
+    for i in transects:
+      transects[i]['transect_proj'] = Tools.convert_epsg(transects[i]['transect'][:,::-1],4326,int(epsg_target))[:][:2]
     
-#%%L8 ini
+    if inputs['TRshp']:
+        toSHP.transectToSHPfile(transects, os.getcwd(), crs=int(epsg_target))
 
-if useL8:
-    L8 = ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA")
-    L8 = L8.filterDate(start,end).filter(ee.Filter.bounds(polygon_tmp)).filter(ee.Filter.lt('CLOUD_COVER',inputs['MaxCloudCover']))
-    if inputs['SpecSeasonalPeriod']:
-        L8 = L8.filter(ee.Filter.calendarRange(inputs['SpecDates'][0],inputs['SpecDates'][1],'month'))
-    L8 = L8.map(GEE.normalizeBandNamesL8)
-    L8 = GEE.resampleL7L8L9(L8, inputs)
+elif inputs['CreateTransects']:
+    wl_composite, wl_noproj_composite = Segmentation.composite_image(inputs,epsg=epsg_target,len_min=1000)
+    transects = Tools.transectsFromComposite(wl_composite,epsg=epsg_target,hspace=inputs['TransectSpacing'])
 
-    if inputs['Waterline']:
-        wl_data=wl_data.merge(L8.map(wl_f).map(ensureProjLandsat))
-    if inputs['SandBar']:
-        sb_data=sb_data.merge(L8.map(sb_f))
-    if inputs['Vegetation']:
-        vg_data.merge(L8.map(vg_f))
-    if inputs['RGB']:
-        rgb_data = rgb_data.merge(L8.map(GEE.RGB).map(ensureProjLandsat))
-#%%L5 ini
-
-if useL5:
-    L5 = ee.ImageCollection("LANDSAT/LT05/C02/T1_TOA")
-    L5 = L5.filterDate(start,end).filter(ee.Filter.bounds(polygon_tmp)).filter(ee.Filter.lt('CLOUD_COVER',inputs['MaxCloudCover']))
-    if inputs['SpecSeasonalPeriod']:
-        L5 = L5.filter(ee.Filter.calendarRange(inputs['SpecDates'][0],inputs['SpecDates'][1],'month'))
-    L5 = L5.map(GEE.normalizeBandNamesL5)
-    L5 = GEE.resampleL5S2(L5,inputs)
-    
-    
-    if inputs['Waterline']:
-        wl_data=wl_data.merge(L5.map(wl_f).map(ensureProjLandsat))
-    if inputs['SandBar']:
-        sb_data=sb_data.merge(L5.map(sb_f))
-    if inputs['Vegetation']:
-        vg_data.merge(L5.map(vg_f))
-    if inputs['RGB']:
-        rgb_data = rgb_data.merge(L5.map(GEE.RGB).map(ensureProjLandsat))
-#%%L7 ini
-
-if useL7:
-    L7 = ee.ImageCollection("LANDSAT/LE07/C02/T1_TOA")
-    L7 = L7.filterDate(start,end).filter(ee.Filter.bounds(polygon_tmp)).filter(ee.Filter.lt('CLOUD_COVER',inputs['MaxCloudCover']))
-    if inputs['SpecSeasonalPeriod']:
-        L7 = L7.filter(ee.Filter.calendarRange(inputs['SpecDates'][0],inputs['SpecDates'][1],'month'))
-    L7 = L7.map(GEE.normalizeBandNamesL7)
-    L7 = GEE.resampleL7L8L9(L7, inputs)
-    
-    if inputs['Waterline']:
-        wl_data=wl_data.merge(L7.map(wl_f).map(ensureProjLandsat))
-    if inputs['SandBar']:
-        sb_data=sb_data.merge(L7.map(sb_f))
-    if inputs['Vegetation']:
-        vg_data.merge(L7.map(vg_f))
-    if inputs['RGB']:
-        rgb_data = rgb_data.merge(L7.map(GEE.RGB).map(ensureProjLandsat))
-#%%L9 ini
-
-if useL9:
-    L9 = ee.ImageCollection("LANDSAT/LC09/C02/T1_TOA")
-    L9 = L9.filterDate(start,end).filter(ee.Filter.bounds(polygon_tmp)).filter(ee.Filter.lt('CLOUD_COVER',inputs['MaxCloudCover']))
-    if inputs['SpecSeasonalPeriod']:
-        L9 = L9.filter(ee.Filter.calendarRange(inputs['SpecDates'][0],inputs['SpecDates'][1],'month'))
-    L9 = L9.map(GEE.normalizeBandNamesL9)
-    L9 = GEE.resampleL7L8L9(L9, inputs)
-    
-    if inputs['Waterline']:
-        wl_data=wl_data.merge(L9.map(wl_f).map(ensureProjLandsat))
-    if inputs['SandBar']:
-        sb_data=sb_data.merge(L9.map(sb_f))
-    if inputs['Vegetation']:
-        vg_data.merge(L9.map(vg_f))
-    if inputs['RGB']:
-        rgb_data = rgb_data.merge(L9.map(GEE.RGB).map(ensureProjLandsat))
-
-#%% IMAGE SAVING ON THE LOCAL COMPUTER/SERVER
-if inputs['Waterline']:
-    sizeDataset = wl_data.size()
-elif inputs['SandBar']:
-    sizeDataset = sb_data.size()
-elif inputs['Vegetation']:
-    sizeDataset = sb_data.size()
-    
-
-
-
-#wl_data = wl_data.map(ensureProj)
-
-if inputs['Waterline']:
-    wl_list=wl_data.toList(sizeDataset)
-    filepath_wl = os.path.join(os.getcwd(),inputs['WaterlineIndex'])
-if inputs['SandBar']:
-    sb_list = sb_data.toList(sizeDataset)
-    filepath_sb = os.path.join(os.getcwd(),inputs['SandBarIndex'])
-if inputs['Vegetation']:
-    vg_list=vg_data.toList(sizeDataset)
-    filepath_vg = os.path.join(os.getcwd(),inputs['VegetationIndex'])
-if inputs['RGB']:
-    rgb_list=rgb_data.toList(sizeDataset)
-    filepath_rgb = os.path.join(os.getcwd(),'RGB')
-length=sizeDataset.getInfo()
-
-#CREATE A .CSV FILE TO CHECK THE COLLECTION OF IMAGES
-records = Tools.generate_attributes(wl_data.getInfo()['features'])
-Tools.upsert_satellite_csv(records)
-
-for i in range(length):
-    
-    if inputs['Waterline']:
-        img = ee.Image(wl_list.get(i))
+OTSU=[]
+sat = []
+dates = []
+list_img = os.listdir(path_index)
+list_img.sort()
+c=-1
+count=0
+for i in list_img:
+  if i[-3:] == 'tif':
+        c+=1
+        if c%10==0 and ploting:
+            plting=True
+        else:
+            plting=False
+        print(i)
         
+        year = int(i[:4])
+        month = int(i[4:6])
+        day = int(i[6:8])
+        hour = int(i[9:11])
+        minute = int(i[11:13])
+        
+        date = datetime.datetime(year,month,day,hour,minute)
+        
+        
+        path_tmp_index = os.path.join(path_index,i)
+        img = gdal.Open(path_tmp_index)
         try:
-            GEE.saveImage(img, i, length, filepath_wl, inputs['WaterlineIndex'], polygon_geom)
+            INDEX = img.ReadAsArray()
         except:
             continue
+        lflat = len(INDEX.flatten())
+        idx_nan=len(np.arange(lflat)[(INDEX==0.0).flatten()])
+        INDEX[np.logical_or((INDEX==0.0),np.isinf(INDEX))]=np.nan
+        rate = idx_nan/lflat
+        if rate<0.5:
+            gt = img.GetGeoTransform()
+            if inputs['Contouring'] == 'RefOtsu' :
+              t_otsuref,t_otsu,valid = Segmentation.refinedOtsu(INDEX, tag_idx, ploting=plting, id_image=i)
+              if not valid:
+                print('Noisy : failed the quality check')
+                count+=1
+                continue
+            elif inputs['Contouring'] == 'WP' :
+              t_otsuref,t_otsu,valid = Segmentation.weightedPeaks(INDEX, tag_idx, ploting=plting)
+              if not valid:
+                print('Noisy : failed the quality check')
+                count+=1
+                continue
+            elif inputs['Contouring'] == 'MSV' :
+              t_otsuref,t_otsu,valid = Segmentation.MSV(INDEX, tag_idx, ploting=plting)
+              if not valid:
+                print('Noisy : failed the quality check')
+                count+=1
+                continue
+            elif inputs['Contouring'] == 'Otsu' :
+              t_otsu, valid = Segmentation.otsu(INDEX, tag_idx, ploting=plting)
+              if not valid:
+                print('Noisy : failed the quality check')
+                count+=1
+                continue
+            wl_tmp, wl_noproj_tmp = Segmentation.getWaterline(INDEX,t_otsu,gt,inputs,i=i)
+            if lag_corr:
+                wl_tmp[:,0] += lags[i[16:18]]
+                wl_tmp[:,1] -= lags[i[16:18]]
+            
+            if inputs['WLshp']:
+                toSHP.waterlineToSHPfile(wl_tmp, tag_idx, seg_method, i, os.getcwd(), crs=int(epsg_target))
+
+            waterline.append(wl_tmp)
+            OTSU.append(t_otsu)
+            dates.append(date)
+            sat.append(i[16:18])
+
+if inputs['NatureROI']=='Transects':
+    for key in transects:
+        transects[key][tag_idx]=dict()
+        transects[key][tag_idx]['raw']=dict()
+        transects[key][tag_idx]['raw']['sat_dates'] = np.array(dates)
+        transects[key][tag_idx]['raw']['sat_missions'] = np.array(sat)
+        transects[key][tag_idx]['raw']['index_threshold'] = np.array(OTSU)
     
-    if inputs['SandBar']:
-        img = ee.Image(sb_list.get(i))
-        GEE.saveImage(img, i, length, filepath_sb, inputs['SandBarIndex'], polygon_geom)
+    transects = Segmentation.computeIntersection(waterline,transects,sat,inputs)
     
-    if inputs['Vegetation']:
-        img = ee.Image(vg_list.get(i))
-        GEE.saveImage(img, i, length, filepath_sb, inputs['VegetationIndex'], polygon_geom)
+    for i in transects:
+        transects[i][tag_idx]['raw'] = Tools.removeNaN(transects[i][tag_idx]['raw'],varname='SDW_'+inputs['WaterlineIndex'])
+    
+    pickle.dump(transects,open('transects_test.p','wb'))
+
+if inputs['WLshp']:
+    
+    folder = os.path.join(os.getcwd(),"WL_"+inputs['WaterlineIndex'],inputs['Contouring'])
+    files = glob.glob(os.path.join(folder,"*.shp"))
+    
+    gdfs = []
+    
+    for shp in files:
+        gdf = gpd.read_file(shp)
+        name = os.path.splitext(os.path.basename(shp))[0]
+        # Add a field identifying the source shapefile
+        gdf["source"] = name
+        gdf["month"] = name[4:6]
+        gdf["year"] = name[:4]
+        gdfs.append(gdf)
+    
+    merged = gpd.GeoDataFrame(
+        pd.concat(gdfs, ignore_index=True),
+        crs=gdfs[0].crs
+    )
+    
+    merged.to_file("Merged_WL_"+inputs['WaterlineIndex']+"_"+inputs['Contouring']+".shp")
+
+
+
         
-    if inputs['RGB']:
-        img = ee.Image(rgb_list.get(i))
-        GEE.saveImage(img, i, length, filepath_rgb, ['red','green','blue'], polygon_geom)
-
-#UPDATE THE .CSV FILE TO INFORM ON THE PROGRESS OF DOWNLOADED IMAGES
-records = Tools.generate_attributes(wl_data.getInfo()['features'])
-Tools.upsert_satellite_csv(records)
-
 
         
 
-        
 
 
